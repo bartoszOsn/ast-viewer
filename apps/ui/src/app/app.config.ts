828ee3021ee006cb2d161e973847fa608512239f
Bartosz Osiński <osinskibartosz24@gmail.com>
2024-06-09 19:10:21 +0200

    setup codemirror for typescript

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { Store } from '../domain/Store';
 import { StoreImpl } from '../domain/StoreImpl';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { CodeEditorModule } from '@ngstack/code-editor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
 	  { provide: Store, useClass: StoreImpl },
 	  provideAnimations(),
-	  provideHttpClient()
+	  provideHttpClient(),
+	  importProvidersFrom(CodeEditorModule.forRoot())
   ]
 };
--- apps/ui/src/ui/code-editor/CodeEditorComponent.ts
+++ apps/ui/src/ui/code-editor/CodeEditorComponent.ts
@@ -1,20 +1,47 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, computed, inject, Signal, ViewChild } from '@angular/core';
 import { Store } from '../../domain/Store';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CodeEditorModule, CodeEditorComponent as NgStackCodeEditorComponent } from '@ngstack/code-editor';
+import { filter, map } from 'rxjs';
 
 @Component({
 	selector: 'app-code-editor',
 	templateUrl: './CodeEditorComponent.html',
 	styleUrl: './CodeEditorComponent.scss',
 	standalone: true,
+	imports: [
+		CodeEditorModule
+	]
 })
-export class CodeEditorComponent {
+export class CodeEditorComponent implements AfterViewInit {
+	@ViewChild('codeEditor', { static: true })
+	codeEditor!: NgStackCodeEditorComponent;
+
 	readonly store = inject(Store);
 
-	readonly code = toSignal(this.store.code$);
+	readonly code = toSignal(this.store.code$, { initialValue: ''});
 
-	onInput(event: Event) {
-		const target = event.target as HTMLTextAreaElement;
-		this.store.setCode(target.value);
+	readonly codeModel: Signal<NgStackCodeEditorComponent['codeModel']> = toSignal(
+		this.store.code$
+			.pipe(
+				filter(() => !!this.codeEditor?.editor),
+				filter(code => code != this.codeEditor.editor.getValue()),
+				map(code => ({
+					language: 'typescript',
+					value: code,
+					uri: 'main.ts'
+				}))
+			),
+		{ initialValue: { language: 'typescript', value: '', uri: 'main.ts' } }
+	)
+
+	readonly options: NgStackCodeEditorComponent['options'] = {}
+
+	ngAfterViewInit(): void {
+		setInterval(() => this.codeEditor.onResize());
+	}
+
+	onCodeChanged(newCode: string) {
+		this.store.setCode(newCode);
 	}
 }
